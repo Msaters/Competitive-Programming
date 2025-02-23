efb256647cfadea4f36daefde958a79109eede56
Milo <msaters123@gmail.com>
2025-02-23 08:40:20 +0100

    CF C. Registration sysyem

---
+++ codeforces/PracticeTasks/C. Registration system.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+unordered_map<string, int> umap;
+
+int main() {
+    int n; cin >> n;
+    while(n--) {
+        string a; cin >> a; int hashedValue = umap[a];
+        if(hashedValue == 0) {
+            cout << "OK";
+        } else {
+            cout << a + to_string(hashedValue);
+        }
+        umap[a]++;
+        cout << endl;
+    }
+
+    return 0;
+}
