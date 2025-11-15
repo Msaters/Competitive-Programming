aabdfa069660ab4a9dd13be03887648c1c7448bf
Milosz <85848694+Msaters@users.noreply.github.com>
2025-11-15 18:32:27 +0100

    Update 2156C - Maximum GCD on Whiteboard.cpp

--- codeforces/PracticeTasks/2156C - Maximum GCD on Whiteboard.cpp
+++ codeforces/PracticeTasks/2156C - Maximum GCD on Whiteboard.cpp
@@ -5,25 +5,31 @@ using ll = long long;
 
 const int N = 2e5 + 7; 
 int arr[N];
+int prefArr[N];
 
 bool canMakeItGcd(int n, int k, int g) {
-    for(int i = 0; i < n; i++) {
-        if(arr[i]%g != 0 && arr[i] < 4*g + arr[i]%g) {
-            k--;
-            if(k < 0) return false;
-        }
-    }
-    return true;
+    int withGcd = prefArr[g] - prefArr[g-1];
+    
+    if(n >= g*2) withGcd += prefArr[g*2] - prefArr[g*2-1];
+    if(n >= g*3) withGcd += prefArr[3*g] - prefArr[3*g - 1];
+    if(n >= g*4) withGcd += prefArr[n] - prefArr[4*g-1];
+    return withGcd >= n - k;
 }
 
 int main() {
     int t; cin >> t;
     while(t) {
         int n, k; cin >> n >> k;
+        for(int i = 0; i <= n; i++) prefArr[i] = 0;
+        
         for(int i = 0; i < n; i++) {
             cin >> arr[i];
+            prefArr[arr[i]]++;
         }
-
+        
+        for(int i = 1; i <= n; i++) {
+            prefArr[i] += prefArr[i-1];
+        }
         
         for(int i = n; i >= 1; i--) {
             if(canMakeItGcd(n, k, i)) {
