3d5bac97282ee7bb9cec36dfef1fdd4232751c6f
Milosz <85848694+Msaters@users.noreply.github.com>
2026-04-05 16:49:29 +0200

    Create 1893. Check if All the Integers in a Range Are Covered.cpp

---
+++ leetCode/Problemsets/sweep line/1893. Check if All the Integers in a Range Are Covered.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isCovered(vector<vector<int>>& ranges, int left, int right) {
+        sort(ranges.begin(), ranges.end());
+        for(int i = 0; i < ranges.size(); i++) {
+            if(left >= ranges[i][0] && left <= ranges[i][1]) {
+                left = ranges[i][1] + 1;
+            }
+        }
+        return left > right;
+    }
+};
