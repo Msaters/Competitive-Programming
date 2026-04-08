f60f996cd5ede7e33f518b4a0b909a79f48b79e6
Milosz <85848694+Msaters@users.noreply.github.com>
2026-04-08 18:43:42 +0200

    Create 452. Minimum Number of Arrows to Burst Balloons.cpp

---
+++ leetCode/Problemsets/sweep line/452. Minimum Number of Arrows to Burst Balloons.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        vector<tuple<int, int, int, int>> Entries;
+        map<pair<int, int>, bool> poped;
+        for(auto point : points) {
+            Entries.push_back({point[0], -1, point[0], point[1]});
+            Entries.push_back({point[1], 1, point[0], point[1]});
+        }
+
+        sort(Entries.begin(), Entries.end());
+        stack<pair<int, int>> S;
+        int result = 0; int currBallons = 0;
+        for(auto entry : Entries) {
+            auto [c, v, i, j] = entry;
+            //printf("[%d %d %d %d]: %d  ", c, i, j, v, poped[{i, j}]);
+            if(!poped[{i, j}] && currBallons > 0 && v == 1) {
+                result++;
+                currBallons = 0;
+                while(!S.empty()) {
+                    auto [l, r] = S.top();
+                    S.pop();
+                    poped[{l, r}] = true;
+                }
+            }
+
+            if(v == -1) {
+                currBallons++;
+                S.push({i, j});
+            }
+        }
+        return result;
+    }
+};
